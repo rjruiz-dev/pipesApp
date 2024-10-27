c2da0fadaca8bf9386fe2f808da90b3601124791
rjruiz <rjruizsf@gmail.com>
2024-10-27 10:08:50 -0300

    AsyncPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -41,5 +42,19 @@ export class UncommonPageComponent {
     age: 36,
     address: 'Ottawa, Canada'
   }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap: ', value))
+  );
+
+  public promiseValue: Promise<string> = new Promise ( (resolve, reject) => {
+    setTimeout( () => {
+      resolve( 'Tenemos data en la promesa')
+      console.log( 'Tenemos data en la promesa')
+      this.person.name = 'Otro nombre'
+    }, 3500);
+  });
+
 
 }
