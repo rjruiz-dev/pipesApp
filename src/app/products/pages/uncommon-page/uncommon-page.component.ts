d1da78ac8a05458854f4c060eb9562673a4561e6
rjruiz <rjruizsf@gmail.com>
2024-10-27 09:53:13 -0300

    KeyValuePipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -35,4 +35,11 @@ export class UncommonPageComponent {
     this.clients.shift();
   }
 
+  // KeyValue Pipe
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada'
+  }
+
 }
