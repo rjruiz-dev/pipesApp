7cad2d0acb958327b80d52ccfe5b681c08d06e9e
rjruiz <rjruizsf@gmail.com>
2024-10-27 09:02:21 -0300

    I18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Rodrigo';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    'male' : 'invitarlo',
+    'female' : 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Melisa';
+    this.gender = 'female';
+  }
+
 }
